820e3da46ecbaa5f499b0f5d48d2a3a9303e7f07
TheIPM <islamluse@gmail.com>
2022-12-17 13:07:21 +1030

    added global vars for capital letters, lowercase letters, numbers and special chars

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
 
+var generateBtn = document.querySelector("#generate");
+var capital = ["A","B","C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"] 
+var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u","v", "w", "x", "y", "z"] 
+var numbers = [0,1,2,3,4,5,6,7,8,9]
+var special = ["!","@","#","$","%","^","&","*","(",")","-","_","+","=","{","[","}","]", ",", "<", ".", ">", "/", "?", ":", ";", "'", "|"]
+
+var passwordLength;
+var uppercaseLetters;
+var lowercaseLetters;
+var numbers;
+var specialCharacters;
+
+console.log(capital,lower,numbers,special)
+
+function passwordQuestions() {
+  resetApp();
+  passwordLength = prompt('How long would you like your password to be? Choose a number between 8 - 128.');
+  uppercaseLetters = confirm('Would you like uppercase letters?');
+  lowercaseLetters = confirm('Would you like lowercase letters?');
+  numbers = confirm('Would you like numbers?');
+  specialCharacters = confirm('Would you like special characters?');
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
