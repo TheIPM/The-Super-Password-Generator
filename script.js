7c70e1cebc2c48e2952d75f0a62335d8536babe3
TheIPM <islamluse@gmail.com>
2022-12-18 13:57:25 +1030

    adding more global vars to consolidate into the function questions

--- script.js
+++ script.js
@@ -6,13 +6,18 @@ var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n
 var numbers = [0,1,2,3,4,5,6,7,8,9]
 var special = ["!","@","#","$","%","^","&","*","(",")","-","_","+","=","{","[","}","]", ",", "<", ".", ">", "/", "?", ":", ";", "'", "|"]
 
-var length
+var password= []; 
+var length;
+var upper1;
+var lower1;
+var numbers1;
+var special1;
 
 
 console.log(capital,lower,numbers,special)
 
 function questions () {
-  prompt("Choose a number between 8 - 128.");
+  length = prompt("Choose a number between 8 - 128.");
   capital = confirm("You want capital letters?");
   lower = confirm("You want lower case letters?");
   numbers = confirm("You want numbers?");
@@ -22,9 +27,18 @@ function questions () {
     alert("Password needs to be between 8 and 128 characters.");
     questions();
   }
+
+  if (!capital && !lower && !numbers && !special) {
+    alert("Need at least one type of character.");
+    questions();
+  }
+
+  return questions;
 }
 
 console.log(questions)
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -33,6 +47,13 @@ function writePassword() {
 
   passwordText.value = password;
 
+}
+
+function makePassword() {
+  var = questions();
+  for (var i = 0; i < l; ++i) {
+    var password += characters.charAt(Math.floor(Math.random() * characters.length));
+}
 }
 
 // Add event listener to generate button
